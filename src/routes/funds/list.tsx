34d46f5b91c61f1bb08f05b20d85809e8282f661
Aditya Mishra <adityamishra5889@gmail.com>
2024-09-04 01:50:19 +0530

    feat:fundsExport (need attention)

--- src/routes/funds/list.tsx
+++ src/routes/funds/list.tsx
@@ -1,12 +1,34 @@
 import React from "react";
-import { List } from "@refinedev/antd";
-import { useGetIdentity, useGo, useOne } from "@refinedev/core";
+import { ExportButton, List } from "@refinedev/antd";
+import { useExport, useGetIdentity, useGo, useOne } from "@refinedev/core";
 import { Database } from "@/utilities";
 import { TransactionList } from "./components/transaction-list";
 import { Button } from "antd";
+import dayjs from "dayjs";
 
 export const FundsList = ({ children }: { children?: React.ReactNode }) => {
-const go = useGo();
+  const go = useGo();
+  const { isLoading: exportLoading, triggerExport } = useExport({
+    resource: "transfers",
+    download: true,
+    onError(error) {
+      console.error(error);
+    },
+    mapData: (record) => {
+      return {
+        //YASH
+        // To: record.to_user_id,
+        // From: record.from_user_id,
+        // Amount: record.amount,
+        // Description: record.description,
+        // Status: record.status,
+        // Created_At: dayjs(record.created_at).format("DD-MM-YYYY"),
+      };
+    },
+    exportOptions: {
+      filename: "orders",
+    },
+  });
   const { data: user } = useGetIdentity<any>();
   const { data: funds, isLoading: isFundsLoading } = useOne<
     Database["public"]["Tables"]["funds"]["Row"]
@@ -23,8 +45,12 @@ const go = useGo();
         title: `Total : ${funds?.data?.total ? funds?.data?.total : "-"}`,
       }}
       headerButtons={[
-        <Button type="primary" onClick={() => go({ to: "requested" })}>Requested</Button>
-      ]} canCreate={false}
+        <Button type="primary" onClick={() => go({ to: "requested" })}>
+          Requested
+        </Button>,
+        <ExportButton onClick={triggerExport} loading={exportLoading} />,
+      ]}
+      canCreate={false}
     >
       <TransactionList userId={user.id} />
       {children}
