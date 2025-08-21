58f20edb60f33d5fb83fa5b51a404ec999dfba1d
yash-pede <yashpede@ssipmt.com>
2025-08-21 22:51:41 +0530

    bugfix: restoring vite pwa

--- vite.config.ts
+++ vite.config.ts
@@ -8,52 +8,52 @@ export default defineConfig({
   plugins: [
     tsconfigPaths({ root: __dirname }),
     react(),
-    // VitePWA({
-    //   registerType: "autoUpdate",
-    //   injectRegister: "auto",
-    //   devOptions: {
-    //     enabled: true,
-    //   },
-    //   workbox: {
-    //     maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MB
-    //   },
-    //   manifest: {
-    //     name: "Admin - PurePride",
-    //     short_name: "Admin - pp",
-    //     description: "Admin application for purepride",
-    //     theme_color: "#ffffff",
-    //     icons: [
-    //       {
-    //         src: "pwa-192x192.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "pwa-maskable-192x192.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //         purpose: "maskable",
-    //       },
-    //       {
-    //         src: "pwa-512x512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "pwa-512x512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //         purpose: "any",
-    //       },
-    //       {
-    //         src: "pwa-maskable-512x512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //         purpose: "maskable",
-    //       },
-    //     ],
-    //   },
-    // }),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MB
+      },
+      manifest: {
+        name: "Admin - PurePride",
+        short_name: "Admin - pp",
+        description: "Admin application for purepride",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
   ],
   build: {
     rollupOptions: {
